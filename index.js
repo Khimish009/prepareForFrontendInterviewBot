d87d22daa365afcfc02f985749ef351ed0d276b6
Khimish009 <salman.khimishev01@gmail.com>
2023-10-05 20:58:01 +0300

    feat: add nodemon

--- index.js
+++ index.js
@@ -3,4 +3,9 @@ const { Bot } = require('grammy')
 
 const bot = new Bot(process.env.BOT_API_KEY)
 
+bot.command('start', (ctx) => {
+    console.log(ctx)
+    ctx.reply('Привет!. Я помогу тебе подготовиться к собесу!!!')
+})
+
 bot.start()
